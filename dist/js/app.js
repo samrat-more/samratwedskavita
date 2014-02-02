76400c461627453649bce2c49b434c86544bc41b
samrat more <samratmore.edu@gmail.com>
2014-02-02 10:38:59 +0530

    Initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,40 @@
+(function(){
+  $(document).ready(function(){
+    // Counter code starts
+    var note = $('#note'),
+    ts = new Date(2014, 2, 9),
+    newYear = true;
+  
+    if((new Date()) > ts){
+      // The new year is here! Count towards something else.
+      // Notice the *1000 at the end - time must be in milliseconds
+      ts = (new Date()).getTime() + 10*24*60*60*1000;
+      newYear = false;
+    }
+    
+    $('#countdown').countdown({
+      timestamp : ts,
+      callback  : function(days, hours, minutes, seconds){
+        
+        var message = "";
+        
+        message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+        message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+        message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+        message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+        
+        if(newYear){
+          message += "left until the new year!";
+        }
+        else {
+          message += "left to 10 days from now!";
+        }
+        
+        note.html(message);
+      }
+    });
+    // Counter code ends
+    // var h = $('.core').outerHeight()
+    // $('.core').css
+  });
+}());
